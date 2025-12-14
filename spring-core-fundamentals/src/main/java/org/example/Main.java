7468cfc6036eec1a02f6370680aebc3200cbe541
harsha <karlakunta.sriharsha@gmail.com>
2025-12-14 12:31:09 +0530

    Update Main loop to 200

--- spring-core-fundamentals/src/main/java/org/example/Main.java
+++ spring-core-fundamentals/src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         System.out.println();
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 200; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
